da4594f24377151a6334d9f3d10b4fd8f248a726
Pranav Ballaney <ballaneypranav@gmail.com>
2020-06-14 02:41:16 +0530

    Reorganize to add /Description and add sticky Table of Contents

---
+++ src/js/content.js
@@ -0,0 +1,1 @@
+import "../css/content.scss";
--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,43 @@ var HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         vendor: "./src/js/vendor.js",
-        main: "./src/index.js"
+        main: "./src/js/main.js",
+        index: "./src/index.js",
+        content: "./src/js/content.js"
     },
     devtool: "none",                            // avoid eval statements
-    plugins: [new HTMLWebpackPlugin({
-        template: "./src/index.pug"
-    })],
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: "./src/index.pug",
+            excludeChunks: ['content']
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'Description/index.html',
+            template: "./src/contents/Description.pug",
+            excludeChunks: ['index']
+        })
+        // new HTMLWebpackPlugin({
+        //     filename: 'Design/index.html',
+        //     template: "./src/contents/Design.pug"
+        // }),
+        // new HTMLWebpackPlugin({
+        //     filename: 'Results/index.html',
+        //     template: "./src/contents/Results.pug"
+        // }),
+        // new HTMLWebpackPlugin({
+        //     filename: 'Contribution/index.html',
+        //     template: "./src/contents/Contribution.pug"
+        // }),
+        // new HTMLWebpackPlugin({
+        //     filename: 'Engineering/index.html',
+        //     template: "./src/contents/Engineering.pug"
+        // }),
+        // new HTMLWebpackPlugin({
+        //     filename: 'Experiments/index.html',
+        //     template: "./src/contents/Experiments.pug"
+        // })
+    ],
     module: {
         rules: [
             {
