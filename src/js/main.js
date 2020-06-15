fa75b83f5d10e3e3068f1634b208daba28777bd9
Pranav Ballaney <ballaneypranav@gmail.com>
2020-06-15 11:47:08 +0530

    Show/hide navbar on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,8 @@
-var navbar = document.getElementsByClassName('navbar')[0];
+var navbar = document.querySelector('nav');
 var menuToggle = document.getElementById('menuToggle');
-var scrollHeight = 100;
-window.onscroll = function () {
-    "use strict";
-    if ((document.body.scrollTop >= scrollHeight || document.documentElement.scrollTop >= scrollHeight)
-        && menuToggle.checked == false) {
-        navbar.classList.add('nav-colored');
-        navbar.classList.remove('nav-transparent');
-    }
-    else {
-        navbar.classList.add('nav-transparent');
-        navbar.classList.remove('nav-colored');
-    }
-};
+var scrollHeight = 100; // make navbar colored/hidden beyond this
 
+// make navbar transparent when fullscreen menu is opened
 function makeNavbarTransparent() {
     if (menuToggle.checked == true) {
         navbar.classList.add('nav-transparent');
@@ -25,3 +14,60 @@ function makeNavbarTransparent() {
     }
 };
 window.makeNavbarTransparent = makeNavbarTransparent;
+
+
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+const transparent = "nav-transparent";
+const colored = "nav-colored";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll == 0) {
+        body.classList.remove(scrollUp);
+        return;
+    }
+
+    if (menuToggle.checked == false) {
+        if (currentScroll >= scrollHeight) {
+            navbar.classList.add(colored);
+            navbar.classList.remove(transparent);
+        }
+        else {
+            navbar.classList.add(transparent);
+            navbar.classList.remove(colored);
+        }
+
+        if (currentScroll >= 2 * scrollHeight) {
+            if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+                // down
+                body.classList.remove(scrollUp);
+                body.classList.add(scrollDown);
+            } 
+            else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+                // up
+                body.classList.remove(scrollDown);
+                body.classList.add(scrollUp);
+            }
+        }
+    }
+
+    lastScroll = currentScroll;
+});
+
+// change navbar background based on scroll
+// window.onscroll = function () {
+//     "use strict";
+//     if ((document.body.scrollTop >= scrollHeight || document.documentElement.scrollTop >= scrollHeight)
+//         && menuToggle.checked == false) {
+//         navbar.classList.add('nav-colored');
+//         navbar.classList.remove('nav-transparent');
+//     }
+//     else {
+//         navbar.classList.add('nav-transparent');
+//         navbar.classList.remove('nav-colored');
+//     }
+// };
