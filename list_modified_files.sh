14555efe6cd1149e9851de5849b3fb2c8a9a2f96
Pranav Ballaney <ballaneypranav@gmail.com>
2020-06-23 09:31:41 +0530

    Allow modifications only to pages/ and img/ via PRs

--- list_modified_files.sh
+++ list_modified_files.sh
@@ -5,6 +5,9 @@ set -e
 CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  echo $CHANGED_FILE
+  if ! [[ $CHANGED_FILE =~ ^src\/(assets\/img\/|pages\/) ]]; then
+    echo You have modified $CHANGED_FILE.
+    echo "Please don't modify files outside src/assets/img/ and src/pages/" 
+    exit 1
+  fi
 done
-
