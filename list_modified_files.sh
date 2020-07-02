8e058a64366e40f7df36edee9014470f3d7a1cda
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-03 01:38:57 +0530

    Test only in PR builds

--- list_modified_files.sh
+++ list_modified_files.sh
@@ -10,6 +10,7 @@ echo $TRAVIS_BRANCH
 # from https://dev.to/ahferroin7/skip-ci-stages-in-travis-based-on-what-files-changed-3a4k
 COMMIT_RANGE="$(echo ${TRAVIS_COMMIT_RANGE} | cut -d '.' -f 1,4 --output-delimiter '..')"
 CHANGED_FILES=
+PR_BUILD="false"
 
 if [ -z "${TRAVIS_COMMIT_RANGE}" ]; then
   echo This is a new branch.
@@ -18,22 +19,23 @@ else
   echo This is not a new branch.
   if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo This is not a PR build.
-
-    echo "This is a 'normal' build."
     CHANGED_FILES="$(git diff --name-only ${COMMIT_RANGE} --)"
   else
     echo This is a PR build.
+    PR_BUILD="true"
     CHANGED_FILES="$(git diff --name-only ${TRAVIS_BRANCH}..HEAD --)"
   fi
 fi
 
 echo $CHANGED_FILES
 
-for CHANGED_FILE in $CHANGED_FILES; do
-  echo $CHANGED_FILE
-  if ! [[ $CHANGED_FILE =~ ^src\/\(assets\/img\/|pages\/\) ]]; then
-    echo You have modified $CHANGED_FILE.
-    echo "Please don't modify files outside src/assets/img/ and src/pages/"
-    exit 1
-  fi
-done
+if [ "${PR_BUILD}" = "true" ]; then
+  for CHANGED_FILE in $CHANGED_FILES; do
+    echo $CHANGED_FILE
+    if ! [[ $CHANGED_FILE =~ ^src\/\(assets\/img\/|pages\/\) ]]; then
+      echo You have modified $CHANGED_FILE.
+      echo "Please don't modify files outside src/assets/img/ and src/pages/"
+      exit 1
+    fi
+  done
+fi
