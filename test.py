f045884107a3640ed8485cc4f9511afc7510eaed
Pranav Ballaney <ballaneypranav@gmail.com>
2020-07-05 13:34:46 +0530

    fstrings don't work for some reason

--- test.py
+++ test.py
@@ -28,14 +28,14 @@ for root, directories, files in os.walk('src/pages'):
 
             # Test: Template line
             if contents[0].strip() != "extends ../templates/contents.pug":
-                print(f"{filename} failed test.")
-                print(f"Template string must be the first line!")
+                print(filename, "failed test.")
+                print("Template string must be the first line!")
                 sys.exit(1)
 
             # Test: headVars line
             if contents[2].strip() != "block headVars":
-                print(f"{filename} failed test.")
-                print(f"`block headVars` missing on line 3.")
+                print(filename, "failed test.")
+                print("`block headVars` missing on line 3.")
                 sys.exit(1)
 
             # Test: Page title
@@ -48,21 +48,21 @@ for root, directories, files in os.walk('src/pages'):
                     properPageTitle = True
                     break
             if not properPageTitle:
-                print(f"{filename} failed test.")
-                print(f"Page title on line 4 doesn't match the filename. Are you sure it's right?")
+                print(filename, "failed test.")
+                print("Page title on line 4 doesn't match the filename. Are you sure it's right?")
                 sys.exit(1)
 
             # Test and remember value of requireMathJax
             requireMathJax = re.split(" ", contents[4].strip())[-1]
             if requireMathJax not in ["true", "false"]:
-                print(f"{filename} failed test.")
-                print(f"Check requireMathJax on line 5. It must be true or false and there must be a space after =.")
+                print(filename, "failed test.")
+                print("Check requireMathJax on line 5. It must be true or false and there must be a space after =.")
                 sys.exit(1)
 
             # Test: article line
             if contents[6].strip() != "block article":
-                print(f"{filename} failed test.")
-                print(f"`block article` missing on line 7.")
+                print(filename, "failed test.")
+                print("`block article` missing on line 7.")
                 sys.exit(1)
 
             imageCount = 0            
@@ -72,8 +72,8 @@ for root, directories, files in os.walk('src/pages'):
                 if "+image" in line:
                     arguments = re.search(r'\((.*?)\)', line)
                     if arguments is None:
-                        print(f"{filename} failed test.")
-                        print(f"Check line {i+1}. Did you provide arguments for the image?")
+                        print(filename, "failed test.")
+                        print("Check line", i+1, ". Did you provide arguments for the image?")
                         sys.exit(1)
                     arguments = arguments.group(1).split(",")
                     
@@ -81,44 +81,44 @@ for root, directories, files in os.walk('src/pages'):
                     try:
                         imageID = int(arguments[0])
                     except ValueError:
-                        print(f"{filename} failed test.")
-                        print(f"Check line {i+1}. Did you provide a number for the image?")
+                        print(filename, "failed test.")
+                        print("Check line", i+1, ". Did you provide a number for the image?")
                         sys.exit(1)
 
                     if imageID != imageCount + 1:
-                        print(f"{filename} failed test.")
-                        print(f"Wrong image number on line {i+1}. Should be {imageCount + 1}.")
+                        print(filename, "failed test.")
+                        print("Wrong image number on line", i+1, ". Should be", imageCount + 1, ".")
                         sys.exit(1)
                     imageCount += 1
                     
                     # Test: Image paths
                     imagePath = re.search(r'\"(.*?)\"', arguments[1])
                     if imagePath is None:
-                        print(f"{filename} failed test.")
-                        print(f"Check line {i+1}. Did you provide an image path?")
+                        print(filename, "failed test.")
+                        print("Check line", i+1, ". Did you provide an image path?")
                         sys.exit(1)
                     imagePath = imagePath.group(1)
                     imagePath = pathlib.Path(imagePath)
                     imagePath = (pathlib.Path('src/assets/img') / imagePath).resolve()
                     if not os.path.exists(imagePath):
-                        print(f"{filename} failed test.")
-                        print(f"Check image path on line {i+1}. Does the file exist?")
+                        print(filename, "failed test.")
+                        print("Check image path on line", i+1, ". Does the file exist?")
                         sys.exit(1)
 
                     # Test: Image captions
                     imageCaption = re.search(r'\"(.*?)\"', arguments[2])
                     if imageCaption is None:
-                        print(f"{filename} failed test.")
-                        print(f"Check image caption on line {i+1}. Did you forget to provide one? Is it within double quotes?")
+                        print(filename, "failed test.")
+                        print("Check image caption on line", i+1, ". Did you forget to provide one? Is it within double quotes?")
                         sys.exit(1)
                     imageCaption = imageCaption.group(1)
                     if imageCaption == "":
-                        print(f"{filename} failed test.")
-                        print(f"Did you forget to provide an image caption on line {i+1}?")
+                        print(filename, "failed test.")
+                        print("Did you forget to provide an image caption on line {i+1}?")
                         sys.exit(1)
                 
                 if "$$" in line:
                     if requireMathJax != "true":
-                        print(f"{filename} failed test.")
-                        print(f"The file appears to contain MathJax on line {i+1} but requireMathJax is not true.")
+                        print(filename, "failed test.")
+                        print("The file appears to contain MathJax on line", i+1, "but requireMathJax is not true.")
                         sys.exit(1)
