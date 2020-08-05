5d9bae6784ca6e8fba12d39393342b9253c5d38c
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-05 09:36:37 +0530

    Make flake8 partially happy

--- test.py
+++ test.py
@@ -58,14 +58,16 @@ for root, directories, files in os.walk('src/pages'):
                     break
             if not properPageTitle:
                 print(filename, "failed test.")
-                print("Page title on line 4 doesn't match the filename. Are you sure it's right?")
+                print(
+                    "Page title on line 4 doesn't match the filename. Are you sure it's right?")
                 sys.exit(1)
 
             # Test and remember value of requireMathJax
             requireMathJax = re.split(" ", contents[4].strip())[-1]
             if requireMathJax not in ["true", "false"]:
                 print(filename, "failed test.")
-                print("Check requireMathJax on line 5. It must be true or false and there must be a space after =.")
+                print(
+                    "Check requireMathJax on line 5. It must be true or false and there must be a space after =.")
                 sys.exit(1)
             containsMathJax = False  # change this later if MathJax is found
 
@@ -83,7 +85,8 @@ for root, directories, files in os.walk('src/pages'):
                     arguments = re.search(r'\((.*?)\)', line)
                     if arguments is None:
                         print(filename, "failed test.")
-                        print("Check line", i+1, ". Did you provide arguments for the image?")
+                        print("Check line", i+1,
+                              ". Did you provide arguments for the image?")
                         sys.exit(1)
                     arguments = arguments.group(1).split(",")
 
@@ -92,12 +95,14 @@ for root, directories, files in os.walk('src/pages'):
                         imageID = int(arguments[0])
                     except ValueError:
                         print(filename, "failed test.")
-                        print("Check line", i+1, ". Did you provide a number for the image?")
+                        print("Check line", i+1,
+                              ". Did you provide a number for the image?")
                         sys.exit(1)
 
                     if imageID != imageCount + 1:
                         print(filename, "failed test.")
-                        print("Wrong image number on line", i+1, ". Should be", imageCount + 1, ".")
+                        print("Wrong image number on line", i+1,
+                              ". Should be", imageCount + 1, ".")
                         sys.exit(1)
                     imageCount += 1
 
@@ -105,33 +110,39 @@ for root, directories, files in os.walk('src/pages'):
                     imagePath = re.search(r'\"(.*?)\"', arguments[1])
                     if imagePath is None:
                         print(filename, "failed test.")
-                        print("Check line", i+1, ". Did you provide an image path?")
+                        print("Check line", i+1,
+                              ". Did you provide an image path?")
                         sys.exit(1)
                     imagePath = imagePath.group(1)
                     imagePath = pathlib.Path(imagePath)
-                    imagePath = (pathlib.Path('src/assets/img') / imagePath).resolve()
+                    imagePath = (pathlib.Path(
+                        'src/assets/img') / imagePath).resolve()
                     if not os.path.exists(imagePath):
                         print(filename, "failed test.")
-                        print("Check image path on line", i+1, ". Does the file exist?")
+                        print("Check image path on line",
+                              i+1, ". Does the file exist?")
                         sys.exit(1)
 
                     # Test: Image captions
                     imageCaption = re.search(r'\"(.*?)\"', arguments[2])
                     if imageCaption is None:
                         print(filename, "failed test.")
-                        print("Check image caption on line", i+1, ". Did you forget to provide one? Is it within double quotes?")
+                        print("Check image caption on line", i+1,
+                              ". Did you forget to provide one? Is it within double quotes?")
                         sys.exit(1)
                     imageCaption = imageCaption.group(1)
                     if imageCaption == "":
                         print(filename, "failed test.")
-                        print("Did you forget to provide an image caption on line {i+1}?")
+                        print(
+                            "Did you forget to provide an image caption on line {i+1}?")
                         sys.exit(1)
 
                 elif "$$" in line:
                     containsMathJax = True
                     if requireMathJax != "true":
                         print(filename, "failed test.")
-                        print("The file appears to contain MathJax on line", i+1, "but requireMathJax is not true.")
+                        print("The file appears to contain MathJax on line",
+                              i+1, "but requireMathJax is not true.")
                         sys.exit(1)
 
                 elif "Citations DOI Start" in line:
@@ -149,7 +160,8 @@ for root, directories, files in os.walk('src/pages'):
 
                     if contents[i+2].rstrip() != "        var citations_url = [":
                         print(filename, "failed test on line", i+3)
-                        print("The line after the hyphen after `prepend citations_url` must exactly be:")
+                        print(
+                            "The line after the hyphen after `prepend citations_url` must exactly be:")
                         print("        var citations_url = [")
                         print("with eight spaces in front.")
 
@@ -167,7 +179,8 @@ for root, directories, files in os.walk('src/pages'):
 
                     if contents[i+2].rstrip() != "        var citations_books = [":
                         print(filename, "failed test on line", i+3)
-                        print("The line after the hyphen after `prepend citations_books` must exactly be:")
+                        print(
+                            "The line after the hyphen after `prepend citations_books` must exactly be:")
                         print("        var citations_books = [")
                         print("with eight spaces in front.")
 
@@ -187,7 +200,8 @@ for root, directories, files in os.walk('src/pages'):
 
             if requireMathJax == "true" and not containsMathJax:
                 print(filename, "failed test.")
-                print("requireMathJax is true but the file doesn't seem to contain any MathJax.")
+                print(
+                    "requireMathJax is true but the file doesn't seem to contain any MathJax.")
                 sys.exit(1)
 
             if citations_doi_running:
@@ -197,12 +211,14 @@ for root, directories, files in os.walk('src/pages'):
 
             if citations_url_running:
                 print(filename, "failed test.")
-                print("`prepend citations_url` was found but `Citations URL End` was not.")
+                print(
+                    "`prepend citations_url` was found but `Citations URL End` was not.")
                 sys.exit(1)
 
             if citations_books_running:
                 print(filename, "failed test.")
-                print("`prepend citations_books` was found but `Citations Books End` was not.")
+                print(
+                    "`prepend citations_books` was found but `Citations Books End` was not.")
                 sys.exit(1)
 
             doi_index = 0
@@ -216,7 +232,7 @@ for root, directories, files in os.walk('src/pages'):
                 number = doi.split(". ")[0]
                 try:
                     number = int(number.strip())
-                except:
+                except Exception:
                     print(filename, "failed test.")
                     print("All dois must be numbered like the following:")
                     print("    1. https://doi.org/10.1007/s00484-015-1117-4")
@@ -227,7 +243,7 @@ for root, directories, files in os.walk('src/pages'):
                     print("DOI numbers must start from 1 and be in order.")
                     sys.exit(1)
                 else:
-                    doi_index += 1 
+                    doi_index += 1
 
             # Convert URL citations to JSON and parse
             citations_url_string = ""
@@ -246,8 +262,9 @@ for root, directories, files in os.walk('src/pages'):
                     if key not in item.keys():
                         print(filename, "failed test.")
                         print(key, "key is missing in the following URL citation:")
-                        print(json.dumps(item, indent = 4, sort_keys=True))
-                        print("You can leave the value blank but it must have all the keys.")
+                        print(json.dumps(item, indent=4, sort_keys=True))
+                        print(
+                            "You can leave the value blank but it must have all the keys.")
                         sys.exit(1)
 
                 required_keys = ["accessed", "title", "site_name"]
@@ -255,15 +272,15 @@ for root, directories, files in os.walk('src/pages'):
                     if key not in item.keys():
                         print(filename, "failed test.")
                         print(key, "key is missing in the following URL citation:")
-                        print(json.dumps(item, indent = 4, sort_keys=True))
+                        print(json.dumps(item, indent=4, sort_keys=True))
                         sys.exit(1)
 
                     if item[key] == "":
                         print(filename, "failed test.")
                         print(key, "value is blank in the following URL citation:")
-                        print(json.dumps(item, indent = 4, sort_keys=True))
+                        print(json.dumps(item, indent=4, sort_keys=True))
                         print(key, "cannot have an empty value.")
-                        sys.exit(1)                        
+                        sys.exit(1)
 
             # Convert Books citations to JSON and parse
             citations_books_string = ""
@@ -277,20 +294,18 @@ for root, directories, files in os.walk('src/pages'):
 
             for item in citations_books_dict:
 
-                required_keys = ["authors", "title", "publisher", "year_published"]
+                required_keys = ["authors", "title",
+                                 "publisher", "year_published"]
                 for key in required_keys:
                     if key not in item.keys():
                         print(filename, "failed test.")
                         print(key, "key is missing in the following URL citation:")
-                        print(json.dumps(item, indent = 4, sort_keys=True))
+                        print(json.dumps(item, indent=4, sort_keys=True))
                         sys.exit(1)
 
                     if item[key] == "":
                         print(filename, "failed test.")
                         print(key, "value is blank in the following URL citation:")
-                        print(json.dumps(item, indent = 4, sort_keys=True))
+                        print(json.dumps(item, indent=4, sort_keys=True))
                         print(key, "cannot have an empty value.")
-                        sys.exit(1)  
-            
-
-                
+                        sys.exit(1)
