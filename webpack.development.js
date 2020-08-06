be13939f83134aca27ce0769192bf0ce2ae9d835
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-07 01:14:23 +0530

    Try fixing css relative paths for deeply nested files

--- webpack.development.js
+++ webpack.development.js
@@ -1,15 +1,17 @@
 const path = require("path");
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
 
 module.exports = merge(common, {
     devtool: "none",                            // avoid eval statements
     mode: "development",
+    plugins: [new ExtractCssChunks()],
     module: {
         rules: [
             {
                 test: /\.scss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [ExtractCssChunks.loader, 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(svg|png|jpg|jpeg|gif|ttf|woff2|woff|eot)$/i,
@@ -21,16 +23,16 @@ module.exports = merge(common, {
                             context: path.resolve(__dirname, "src/"),
                             outputPath: ".",
                             publicPath: ".",
-                            useRelativePaths: true,
+                            useRelativePaths: true
                         }
                     },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true, // webpack@1.x
-                            disable: true, // webpack@2.x and newer
-                        },
-                    }
+                    // {
+                    //     loader: 'image-webpack-loader',
+                    //     options: {
+                    //         bypassOnDebug: true, // webpack@1.x
+                    //         disable: true, // webpack@2.x and newer
+                    //     },
+                    // }
                 ]
             }
         ],
