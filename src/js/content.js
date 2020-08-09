85da81a327daf2bcfc3959dbebbf57b9111840cd
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-09 09:40:56 +0530

    Add source link for header images

--- src/js/content.js
+++ src/js/content.js
@@ -69,17 +69,4 @@ $('dfn').each(function () {
     $(this).tooltip();
 });
 
-$('.content .section').tooltip();
-
-$('.content .section').click(function () {
-    $(this)
-        .tooltip('hide')
-        .attr("data-original-title", "Doesn't work yet :/")
-        .tooltip('show');
-});
-
-$('.content .section').mouseout(function () {
-    $(this)
-        .attr("data-original-title", "Copy a link to this section!");
-});
-
+$('#bg-attribution').tooltip();
