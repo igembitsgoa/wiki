30b35725278f9b50942094c449e97f311b5ea9d4
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-13 18:56:28 +0530

    Fix header background image

--- preprocess.py
+++ preprocess.py
@@ -44,6 +44,25 @@ for root, _, files in os.walk('src/pages'):
                     print(f"{img_path} doesn't exist")
                     sys.exit(1)
 
+            elif "var backgroundImage" in line and 'require(' not in line and "['default']" not in line:
+                # split by = and join everything after first =
+                parts = line.split('=')
+                img_name = '='.join(parts[1:])
+                if '"' in img_name:
+                    img_name = parts[1].split('"')[1]
+                elif "'" in img_name:
+                    img_name = parts[1].split("'")[1]
+
+                img_path = Path('src/assets/img') / Path(img_name)
+
+                if img_path.exists():
+                    relative_path = os.path.relpath(img_path, filepath.parent)
+                    levels = relative_path.count('../') - 1
+                    templines.append(f'{parts[0]}= "{"../"*levels}" + require("{relative_path}")["default"]\n')
+
+                else:
+                    print(f"{img_path} doesn't exist")
+                    sys.exit(1)
             else:
                 templines.append(line)
         
