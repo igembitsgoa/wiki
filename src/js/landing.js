b2613fe78236de31470a07a6a169bcf1cc6fbcc9
ameya1101 <ameyathete@icloud.com>
2020-09-20 22:40:10 +0530

    Add dark mode to landing pages

--- src/js/landing.js
+++ src/js/landing.js
@@ -2,3 +2,32 @@ import "../css/content.scss";
 import "../css/landing.scss";
 import "./main";
 
+// sourced from https://stackoverflow.com/questions/50840168/how-to-detect-if-the-os-is-in-dark-mode-in-browsers
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    $('#themeSwitchInput').prop('checked', true);
+    $('body').addClass('dark');
+}
+
+// switch to dark theme based if set in local storage
+$(function(){
+    if (localStorage.getItem('theme') == 'dark') {
+        $('body').addClass('dark');
+        $('#themeSwitchInput').prop('checked', true);
+    }
+    else if (localStorage.getItem('theme') == 'light') {
+        $('body').removeClass('dark');
+        $('#themeSwitchInput').prop('checked', false);
+    }
+});
+
+
+$('label[for="themeSwitchInput"]').click(function() {
+    if ($("#themeSwitchInput").is(":checked")) {
+        $('body').addClass('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        $('body').removeClass('dark');
+        localStorage.setItem('theme', 'light');
+    }
+});
