ed9c7186c52d81d3fb3142a47916b2298cd2693f
Pranav Ballaney <ballaneypranav@gmail.com>
2020-10-20 23:55:47 +0530

    Try to fix video height

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./js/main";
 //     $(window).on('resize', function(){
 //         clearTimeout(window.resizeEvt);
 //         window.resizeEvt = setTimeout(function(){
-        
+
 //             var width = getWidth();
 //             console.log(width);
 //             // alert("width".concat("hi"));
@@ -17,9 +17,26 @@ import "./js/main";
 
 // a terrible way to fix the github.io /wiki/ problem
 if (window.location.href.includes("github.io")) {
-    $("source").each(function () {
-      var link = $(this).attr("src").replace('wiki/..', 'wiki');
-      $(this).attr("href", link);
-    });
+  $("source").each(function () {
+    var link = $(this).attr("src").replace("wiki/..", "wiki");
+    $(this).attr("href", link);
+  });
+}
+
+$(window).on('resize', function () {
+
+  var width = getWidth();
+  var height =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+
+  var ratio = width / height;
+
+  if (ratio < 1920/1080) {
+    $('header').css('height', 'unset');
   }
-  
+  else {
+    $('header').css('height', '100vh');
+  }
+});
