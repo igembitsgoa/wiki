8f089170660320f08543f5898b2586f33e3af028
Pranav Ballaney <ballaneypranav@gmail.com>
2020-10-25 14:25:30 +0530

    Engineering success test

--- src/js/engineering.js
+++ src/js/engineering.js
@@ -108,30 +108,78 @@ if (window.location.href.includes("github.io")) {
   });
 }
 
-$(window).resize(function () {
+function positionArrows() {
   $(".arrow-container").each(function () {
     var from = $(this).attr("data-from");
     var to = $(this).attr("data-to");
 
-    var from_position = $(from).position();
-    var to_position = $(to).position();
-
+    var from_self_position = $(from).position();
     var from_parent_position = $(from).parent().position();
-    var to_parent_position = $(to).parent().position();
     var from_width = $(from).outerWidth();
     var from_height = $(from).outerHeight();
+    
+    var to_self_position = $(to).position();
+    var to_parent_position = $(to).parent().position();
+    var to_height = $(to).outerHeight();
+    var to_width = $(to).outerWidth();
 
-    var top = from_position.top + from_parent_position.top + from_height / 2;
-    var left = from_position.left + from_parent_position.left + from_width - 1;
-    var right = to_position.left + to_parent_position.left;
+    var from_x = from_self_position.left + from_parent_position.left;
+    var from_y = from_self_position.top + from_parent_position.top;
 
-    var line = $(this).find("line");
-    $(this).css("top", top);
-    $(this).css("left", left);
+    var to_x = to_self_position.left + to_parent_position.left;
+    var to_y = to_self_position.top + to_parent_position.top;
 
-    console.log(from_position, width);
-    console.log(from_parent_position, width);
-    console.log(to_position, width);
-    console.log(to_parent_position, width);
+    console.log(from_x, from_y, to_x, to_y);
+
+    if (from_x < to_x) {
+      var x1 = from_x + from_width;
+      var x2 = to_x;
+    }
+    else if (from_x > to_x) {
+      var x1 = to_x + to_width;
+      var x2 = from_x;
+    }
+    else {
+      var x1 = from_x + from_width / 2;
+      var x2 = x1
+    }
+
+    if (from_y < to_y) {
+      var y1 = from_y + from_height / 2;
+      var y2 = to_y + to_height / 2;
+
+      if (from_x == to_x) {
+        y1 += from_height / 2;
+        y2 -= to_height / 2;
+      }
+    }
+    else {
+      var y1 = to_y + to_height / 2;
+      var y2 = from_y + from_height / 2;
+    }
+
+    var width = Math.abs(x2 - x1);
+    var height = Math.abs(y2 - y1);
+
+    $(this).css("left", Math.min(x1, x2));
+    $(this).css("top", Math.min(y1, y2));
+
+    var svg = $(this).find('svg')
+    svg.attr('width', width);
+    svg.attr('height', height);
+
+    var line = $(this).find('line');
+    line.attr('x1', (from_x < to_x ? 0 : width))  + line.attr('offset_x1');
+    line.attr('y1', (from_y < to_y ? 0 : height)) + line.attr('offset_y1');
+    line.attr('x2', (from_x < to_x ? width : 0))  + line.attr('offset_x2');
+    line.attr('y2', (from_y < to_y ? height : 0)) + line.attr('offset_y2');
   });
+};
+
+$(document).ready(function() {
+  positionArrows();
+});
+
+$(window).on('resize', function() {
+  positionArrows();
 });
