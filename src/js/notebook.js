d1d1a61f771f2cda5a6fead7319f342db2102eea
Pranav Ballaney <ballaneypranav@gmail.com>
2020-10-25 19:08:24 +0530

    Notebook mobile changes

--- src/js/notebook.js
+++ src/js/notebook.js
@@ -61,21 +61,39 @@ $("#months li a").on("click", function () {
     });
 
     // reset tab and active classes
-    $(id).find('.tabs li').removeClass('active');
-    $(id).find('.tabs li').first().addClass('active');
-    $(id).find('.week').fadeOut();
-    $(id).find('.week1').fadeIn();
+    $(id).find(".tabs li").removeClass("active");
+    $(id).find(".tabs li").first().addClass("active");
+    $(id).find(".week").fadeOut();
+    $(id).find(".week1").fadeIn();
   }
+  return false;
+});
+
+$("#months-dropdown").on("change", function () {
+  var id = "#" + $(this).find(":selected").val();
+
+  $.when($(".month").fadeOut()).then(function () {
+    $(id).fadeIn();
+    $(id).css('display', 'block');
+  });
+
+  // reset tab and active classes
+  $(id).find(".tabs li").removeClass("active");
+  $(id).find(".tabs li").first().addClass("active");
+  $(id).find(".week").fadeOut();
+  $(id).find(".week1").fadeIn();
+
   return false;
 });
 
 $(".tabs li").on("click", function () {
   if (!$(this).hasClass("active")) {
-    
-    $(this).siblings().each(function() {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
+    $(this)
+      .siblings()
+      .each(function () {
+        $(this).removeClass("active");
+      });
+    $(this).addClass("active");
 
     var week = $(this).find("a").attr("href").slice(-1);
     $.when($(".week").fadeOut()).then(function () {
@@ -85,38 +103,41 @@ $(".tabs li").on("click", function () {
 });
 
 // sourced from https://stackoverflow.com/questions/50840168/how-to-detect-if-the-os-is-in-dark-mode-in-browsers
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  $('#themeSwitchInput').prop('checked', true);
-  $('body').addClass('dark');
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  $("#themeSwitchInput").prop("checked", true);
+  $("body").addClass("dark");
 }
 
 // switch to dark theme based if set in local storage
-$(function(){
-  if (localStorage.getItem('theme') == 'dark') {
-      $('body').addClass('dark');
-      $('#themeSwitchInput').prop('checked', true);
-  }
-  else if (localStorage.getItem('theme') == 'light') {
-      $('body').removeClass('dark');
-      $('#themeSwitchInput').prop('checked', false);
+$(function () {
+  if (localStorage.getItem("theme") == "dark") {
+    $("body").addClass("dark");
+    $("#themeSwitchInput").prop("checked", true);
+  } else if (localStorage.getItem("theme") == "light") {
+    $("body").removeClass("dark");
+    $("#themeSwitchInput").prop("checked", false);
   }
 });
 
-
-$('label[for="themeSwitchInput"]').click(function() {
+$('label[for="themeSwitchInput"]').click(function () {
   if ($("#themeSwitchInput").is(":checked")) {
-      $('body').addClass('dark');
-      localStorage.setItem('theme', 'dark');
-  }
-  else {
-      $('body').removeClass('dark');
-      localStorage.setItem('theme', 'light');
+    $("body").addClass("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    $("body").removeClass("dark");
+    localStorage.setItem("theme", "light");
   }
 });
 
 // apply theme class to body if it's applied to main
 // this is necessary because iGEM deletes all classes applied to body
-var themeClass = $('main').attr('class').split(' ').filter(function(className) {
-  return className.match('theme')
-})
-$('body').addClass(themeClass);
+var themeClass = $("main")
+  .attr("class")
+  .split(" ")
+  .filter(function (className) {
+    return className.match("theme");
+  });
+$("body").addClass(themeClass);
